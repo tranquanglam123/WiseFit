817f51b0752b6e81261c09c5289d991bb9e3e602
quanglam123 <kwanglum815@gmail.com>
2023-06-01 14:37:01 +0700

    June 1st : Config Logout COmplete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {Route, Routes, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Form from './pages/Form';
 import { app } from './utils/Firebase-config';
-
+import { ToastContainer,toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -18,7 +19,10 @@ const App = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    const authToken = sessionStorage.getItem('Auth Token');
+    return authToken !== null;
+  });
   const handleAction = (id) => {
     const authentication = getAuth();
 
@@ -29,7 +33,15 @@ const App = () => {
         navigate('/');
         sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken);
         setIsLoggedIn(true);
-      });
+      })
+    .catch((error) => {
+      if (error.code === 'auth/wrong-password'){
+        toast.error('Please check the password');
+      }
+      if (error.code === 'auth/user-not-found') {
+        toast.error('Please check the email');
+      }
+    })
     }
 
     // Register functionality
@@ -38,26 +50,21 @@ const App = () => {
         navigate('/');
         sessionStorage.setItem('Auth Token', response._tokenResponse.refreshToken);
         setIsLoggedIn(true);
-      });
+      })
+      .catch((error) => {
+        if (error.code === 'auth/email-already-in-use'){
+          toast.error('Email Already in use');
+        }
+      })
     }
   }
-  //token function
-  /*
-  useEffect(() => {
-    let authToken = sessionStorage.getItem('Auth Token');
-
-    // Condition check
-    if (authToken) {
-      navigate('/home');
-    }
-  }, [navigate]);
-*/
   useEffect(() => {
     navigate('/login');
   }, []);
   
   return (
     <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <ToastContainer/>
       {isLoggedIn ? (
         <>
           <Navbar />
@@ -71,18 +78,27 @@ const App = () => {
           <Footer />
         </>
       ) : (
-        <Routes>
-          <Route
-            path="/login"
-            element={<Form title="Login" setEmail={setEmail} setPassword={setPassword} handleAction={() => handleAction(1)} />}
-          />
-          <Route
-            path="/register"
-            element={<Form title="Register" setEmail={setEmail} setPassword={setPassword} handleAction={() => handleAction(2)} />}
-          />
-        </Routes>
-      )}
-    </Box>
+        <>
+      <Routes>
+        <Route
+          path="/login"
+          element={<Form title="Login" setEmail={setEmail} setPassword={setPassword} handleAction={() => handleAction(1)} />}
+        />
+        <Route
+          path="/register"
+          element={<Form title="Register" setEmail={setEmail} setPassword={setPassword} handleAction={() => handleAction(2)} />}
+        />
+        <Route
+          path="/"
+          element={() => {
+            navigate('/login');
+            return null;
+          }}
+        />
+      </Routes>
+    </>
+  )}
+</Box>
   );
 };
 export default App;
--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,11 +1,76 @@
-import React, { useState } from 'react';
-import { Button, Typography, Box } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Button, Typography, Box, Table, TableBody, TableCell, TableRow, TextField } from '@mui/material';
 import RapidoScale  from '../assets/images/scale.jpg';
 import MiBand3  from '../assets/images/1_Mi_Band_3.jpg';
+import {useNavigate } from 'react-router-dom';
+function UserProfile() {
+  let navigate = useNavigate();
+  const [user, setUser] = useState({
+    Name: 'LAM TRAN',
+    profileImage: 'https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-1/248179689_399415275178957_8207686571705545584_n.jpg?stp=dst-jpg_p160x160&_nc_cat=109&ccb=1-7&_nc_sid=7206a8&_nc_ohc=i9G6h_D3eGoAX-m40_x&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfBrqif6QANcC9z6hj2G8KKyoA97RWJehtw6ZXlnwMjBEQ&oe=647DFDE8',
+    information: {
+      Age: '',
+      Email: '',
+      Address: '',
+    },
+  });
+
+  const handleEdit = (field, value) => {
+    setUser(prevUser => ({
+      ...prevUser,
+      information: {
+        ...prevUser.information,
+        [field]: value,
+      },
+    }));
+  };
+
+  return (
+    <Box display='flex' alignItems='center' marginLeft='-50px'>
+    {/* User Profile Image */}
+    <Box marginRight={4}>
+      <img src={user.profileImage} alt='Profile' style={{ width: '300px', height: '300px', borderRadius: '50%', marginTop:'50px' }} />
+      <Typography variant='h4' fontWeight='600' gutterBottom color='#ffffff' marginTop='20px' textAlign='center'>
+        {user.Name}
+      </Typography>
+      
+      {/*<Button onClick={handleLogout}>Log Out</Button>*/}
+    </Box>
+      {/* User Information Table */}
+      <Table style={{ width: '600px', height: '400px' ,marginTop: '20px', marginLeft:'75px' }}>
+        <TableBody>
+          {Object.entries(user.information).map(([key, value]) => (
+            <TableRow key={key}>
+              <TableCell component='th' scope='row' style={{ fontSize: '25px',fontWeight: 'bold', width: '100px', color: '#ffffff' }}>
+                {key}
+              </TableCell>
+              <TableCell>
+                <TextField
+                  value={value}
+                  onChange={event => handleEdit(key, event.target.value)}
+                  fullWidth
+                  variant='outlined'
+                  size='small'
+                  InputProps={{ style: { color: '#ffffff',fontSize:'20px' ,fontWeight: 'bold' } }}
+                  style={{ borderColor: '#ffffff'}}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      </Box>
+  );
+}
+
+
 const Connect = () => {
   const [batteryLevel, setBatteryLevel] = useState(null);
   const [connectedDevice, setConnectedDevice] = useState(null);
-
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    const authToken = sessionStorage.getItem('Auth Token');
+    return authToken !== null;
+  });
   const handleConnect = async () => {
     try {
       const device = await navigator.bluetooth.requestDevice({
@@ -29,7 +94,17 @@ const Connect = () => {
       // Handle connection failure or display an error message
     }
   };
-
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    sessionStorage.removeItem('Auth Token');
+    setIsLoggedIn(false);
+    window.location.reload();
+  };
+  useEffect(() => {
+    const authToken = sessionStorage.getItem('Auth Token');
+    setIsLoggedIn(authToken !== null);
+  }, [isLoggedIn]);
+  
   return (
     <Box
       display='flex'
@@ -38,6 +113,11 @@ const Connect = () => {
       alignItems='center'
       minHeight='100vh'
     >
+    {/*User Profile Content*/}
+    <UserProfile/>
+    <Button onClick={handleLogout}>Log Out</Button>
+
+
       <Typography marginTop='50px' variant='h2' fontWeight='700' gutterBottom color='#ffffff'>
         CONNECT FOR RECOMMENDATION
       </Typography>
